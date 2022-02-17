27b90b99955cb368ea6c20de4bb96fcb5bc8842d
Yandhi <15877410869@163.com>
2022-02-17 23:17:18 +0800

    deploy

---
+++ precache-manifest.2ca3c968ab482c3bedc6b37efd01d184.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbbadba303be43708e3",
+    "url": "/keep-accounts-website/css/app.3a2915fc.css"
+  },
+  {
+    "revision": "e21389165ac68d1ca912",
+    "url": "/keep-accounts-website/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/keep-accounts-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/keep-accounts-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "471ed1b633af64d44d694f66983b8dcf",
+    "url": "/keep-accounts-website/index.html"
+  },
+  {
+    "revision": "6bbbadba303be43708e3",
+    "url": "/keep-accounts-website/js/app.4b86f731.js"
+  },
+  {
+    "revision": "e21389165ac68d1ca912",
+    "url": "/keep-accounts-website/js/chunk-vendors.1c54dd9e.js"
+  },
+  {
+    "revision": "e61f181c386636f57218dd4db74ba26c",
+    "url": "/keep-accounts-website/keep-accounts.png"
+  },
+  {
+    "revision": "b0d6e472d87a1e9620d9bc3c450d5cf3",
+    "url": "/keep-accounts-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-accounts-website/robots.txt"
+  }
+]);
